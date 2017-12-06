d7e75aeb7503fe1003366e439c1f95f23508fe71
joshbridgers <joshbridgers@gmail.com>
2017-12-06 10:40:56 -0500

    python3 fixes, remove fastq symlink

--- dragen_pipe.py
+++ dragen_pipe.py
@@ -14,25 +14,22 @@ from datetime import datetime
 from db_statements import *
 from dragen_sample import dragen_sample
 from glob import glob
-from utilities import * 
+from utilities import *
 
 def main(run_type_flag, debug, dontexecute, database, seqscratch_drive):
     config = get_config()
     if isinstance(run_type_flag,str) == True: #pseudo_prepid present
         pseudo_prepid = run_type_flag
         sample_name, sample_type, pseudo_prepid, capture_kit = get_next_sample(pseudo_prepid,database,debug)
-        print(capture_kit)
         sample = dragen_sample(sample_name,sample_type,pseudo_prepid,capture_kit,database)
-        setup_dir(seqscratch_drive,sample,debug)
-        run_sample(sample,dontexecute,config,database,debug)
+        run_sample(sample,dontexecute,config,seqscratch_drive,database,debug)
 
     elif run_type_flag == True: #Automated run
         pseudo_prepid = 0
         sample_name, sample_type, pseudo_prepid, capture_kit = get_next_sample(pseudo_prepid,database,debug)
         while sample_name is not None:
             sample = dragen_sample(sample_name,sample_type,pseudo_prepid,capture_kit,database)
-            setup_dir(seqscratch_drive,sample,debug)
-            run_sample(sample,dontexecute,config,database,debug)
+            run_sample(sample,dontexecute,config,seqscratch_drive,database,debug)
             """ These two functions will need to be moved to the dragen_release
                 script when available.  For now will run on every bam in scratch
                 folder """
@@ -71,8 +68,9 @@ def check_bam_found_vs_bam_db(sample,qualified_bams_found):
             #print(qualified_bams_found,qualified_bams_in_db)
             raise ValueError("Number of bams found != number of RGs in database!")
         for db_bam in qualified_bams_in_db:
+            print(db_bam)
             db_bam = ("{output_dir}/{sample_name}.{pseudo_prepid}.{fcillumid}.{lane_num}.bam"
-                     ).format(fcillumid=db_bam[0],lane_num=db_bam[1],**sample.metadata)
+                     ).format(fcillumid=db_bam['fcillumid'],lane_num=db_bam['laneNum'],**sample.metadata)
             if glob(db_bam) == []:
                 raise ValueError("Bam {} not found!".format(db_bam))
 
@@ -97,7 +95,7 @@ def is_external_or_legacy_sample(sample):
 
     else:
         query = IS_SAMPLE_EXTERNAL_FROM_PREPID.format(prepid=sample.metadata['prepid'][0])
-        is_external_sample = int(run_query(query,database)[0][0])
+        is_external_sample = int(run_query(query,database)[0]['IS_EXTERNAL'])
         if is_external_sample == 1:
             print("Sample is an external sample!")
             return True
@@ -125,8 +123,10 @@ def write_sge_header(sample,step,script_loc):
     return script
 
 
-def run_sample(sample,dontexecute,config,database,debug):
-    existing_bams_check = True
+def run_sample(sample,dontexecute,config,seqscratch_drive,database,debug):
+    check_Fastq_Total_Size(sample,debug)
+    setup_dir(seqscratch_drive,sample,debug)
+    existing_bams_check = False
     output_dir = sample.metadata['output_dir']
     pseudo_prepid = sample.metadata['pseudo_prepid']
     submit_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -143,13 +143,13 @@ def run_sample(sample,dontexecute,config,database,debug):
             if dontexecute == False:
                run_dragen_on_read_group(sample,rg_fcillumid,rg_lane_num,debug)
             update_lane_metrics(sample,rg_lane_num,rg_fcillumid,rg_prepid,database)
+
+        component_bams = get_component_bams(sample,debug)
+        update_dragen_metadata(sample,component_bams,database,debug)
         rm_query = "DELETE FROM {0} WHERE pseudo_prepid={1}".format("tmp_dragen",pseudo_prepid)
         if debug:
             print(rm_query)
         run_query(rm_query,database)
-
-        component_bams = get_component_bams(sample,debug)
-        update_dragen_metadata(sample,component_bams,database,debug)
 
     else:
         print(("Sample {sample_name} bam file already exists!")
@@ -184,8 +184,10 @@ def run_dragen_on_read_group(sample,rg_fcillumid,rg_lane_num,debug):
     dragen_stderr.close()
     if rc != 0:
         raise Exception("Dragen alignment did not complete successfully!")
-    subprocess.call(['chmod','-R','775','{}'.format(output_dir)])
-
+    try:
+        subprocess.call(['chmod','-R','775','{}'.format(output_dir)])
+    except:
+        pass
 def update_dragen_metadata(sample,component_bams,database,debug):
     seqscratch_drive = sample.metadata['output_dir'].split('/')[2]
     get_dragen_metadata_query = ("SELECT * from dragen_sample_metadata "
@@ -242,7 +244,7 @@ def set_seqtime(rg_fcillumid,sample,database):
             "FROM Flowcell WHERE FCIllumID='{}'").format(rg_fcillumid)
     seqtime = run_query(query,database)
     if seqtime:
-        seqtime = seqtime[0][0].date().isoformat() #ISO8601 format for RGDT field
+        seqtime = seqtime[0]['SEQTIME'].date().isoformat() #ISO8601 format for RGDT field
     else: #In case there is not flowcell information (Ex. old and external samples)
         seqtime = '1970-1-1'
     sample.set('seqtime',seqtime)
@@ -256,7 +258,6 @@ def setup_dir(seqscratch_drive,sample,debug):
                                              pseudo_prepid=sample.metadata['pseudo_prepid'])
     sample.metadata['output_dir'] = output_dir
     sample.metadata['script_dir'] = sample.metadata['output_dir']+'scripts'
-    sample.metadata['fastq_dir'] = sample.metadata['output_dir']+'fastq'
     sample.metadata['log_dir'] = sample.metadata['output_dir']+'logs'
     dragen_stdout = "{log_dir}/{sample_name}.{pseudo_prepid}.dragen.out".format(**sample.metadata)
     sample.metadata['dragen_stdout'] = dragen_stdout
@@ -265,48 +266,8 @@ def setup_dir(seqscratch_drive,sample,debug):
 
     mkdir_cmd = ['mkdir','-p',sample.metadata['script_dir']]
     subprocess.call(mkdir_cmd)
-    print(mkdir_cmd)
     mkdir_cmd = ['mkdir','-p',sample.metadata['log_dir']]
     subprocess.call(mkdir_cmd)
-    mkdir_cmd = ['mkdir','-p',sample.metadata['fastq_dir']]
-    subprocess.call(mkdir_cmd)
-    """Removes any fastq.gz files in the fastq folder. Symlink-only fastqs
-        should be in this folder.  This is just in case the sample was run
-        through once"""
-    symLinkedFastqFiles = glob(str(sample.metadata['fastq_dir']) + '/*fastq.gz')
-    for file in symLinkedFastqFiles:
-        #remove any symlinked fastq.gz files
-        try:
-            os.remove(file)
-        except OSError:
-            pass
-
-    first_read1 = get_reads(sample,1,debug)
-    first_read2 = get_reads(sample,2,debug)
-    fastq_counter=0
-    for fastq_loc in sample.metadata['fastq_loc']:
-        fastqs = glob(fastq_loc + '/*_R1_*fastq.gz')
-        for fastq in fastqs:
-            if 'fastq16-rsync' in fastq:
-                pass
-            else:
-                fastq_counter+=1
-                new_fastq_read1 = first_read1.split('/')[-1].replace(
-                        '001.fastq.gz','{0:03d}.fastq.gz'.format(fastq_counter))
-                new_fastq_read2 = first_read2.split('/')[-1].replace(
-                        '001.fastq.gz','{0:03d}.fastq.gz'.format(fastq_counter))
-                ln_cmd1 = ['ln','-s',fastq,sample.metadata['fastq_dir']+'/'+new_fastq_read1]
-                ln_cmd2 = ['ln','-s',fastq.replace('_R1_','_R2_'),sample.metadata['fastq_dir']+'/'+new_fastq_read2]
-
-                if fastq_counter == 1:
-                    sample.set('first_fastq1',sample.metadata['fastq_dir']+'/'+new_fastq_read1)
-                    sample.set('first_fastq2',sample.metadata['fastq_dir']+'/'+new_fastq_read2)
-                    sample.set('first_lane',sample.metadata['lane'][0][0][0])
-                    sample.set('first_flowcell',sample.metadata['lane'][0][0][1])
-
-                subprocess.call(ln_cmd1)
-                subprocess.call(ln_cmd2)
-    check_Fastq_Total_Size(sample,debug)
 
 def setup_first_read_RG(sample,rg_lane_num,rg_fcillumid,rg_prepid,debug):
     first_read1,first_read2 = get_first_read(sample,rg_lane_num,rg_fcillumid,debug)
@@ -327,45 +288,44 @@ def get_first_read(sample,rg_lane_num,rg_fcillumid,debug):
     second_read = first_read.replace('_R1_','_R2_')
     return first_read,second_read
 
-
 def check_Fastq_Total_Size(sample,debug):
-    fastq_dir = sample.metadata['fastq_dir']
     sample_type = sample.metadata['sample_type']
     fastq_filesize_sum = 0
-    for fastq in glob(fastq_dir + '/*gz'):
-        fastq_filesize = os.stat(os.path.realpath(fastq)).st_size
-        fastq_filesize_sum += fastq_filesize
+    for fastq_loc in sample.metadata['fastq_loc']:
+        for fastq in glob(fastq_loc + '/*gz'):
+            fastq_filesize = os.stat(os.path.realpath(fastq)).st_size
+            fastq_filesize_sum += fastq_filesize
     print("Sum of Fastq size: {}".format(fastq_filesize_sum))
     if sample_type == 'genome':
         if fastq_filesize_sum < 42949672960: # < 40GB
             print(fastq_filesize_sum,float(fastq_filesize_sum)/1024/1024/1024)
-            userInput = raw_input('Sum of fastq files sizes are too small.  Is this ok? (y)es or (n)o ').lower()
+            userInput = input('Sum of fastq files sizes are too small.  Is this ok? (y)es or (n)o ').lower()
             if userInput == 'n':
                 raise Exception("Sum of fastq files sizes is too small for a {} sample!".format(sample_type))
     elif sample_type == 'exome':
         if fastq_filesize_sum > 32212254720: # > 30GB
             print(fastq_filesize_sum,float(fastq_filesize_sum)/1024/1024/1024)
-            userInput = raw_input('Sum of fastq files sizes are too big.  Is this ok? (y)es or (n)o ').lower()
+            userInput = input('Sum of fastq files sizes are too big.  Is this ok? (y)es or (n)o ').lower()
             if userInput == 'n':
                  raise Exception("Sum of fastq files is too big for a {} sample!".format(sample_type))
         elif fastq_filesize_sum < 1073741824: # < 1GB
-            userInput = raw_input('Sum of fastq files sizes are too small.  Is this ok? (y)es or (n)o ').lower()
+            userInput = input('Sum of fastq files sizes are too small.  Is this ok? (y)es or (n)o ').lower()
             if userInput == 'n':
                  raise Exception("Sum of fastq files is too small for a {} sample!".format(sample_type))
     elif sample_type == 'rnaseq':
         if fastq_filesize_sum > 32212254720: # > 30GB
             print(fastq_filesize_sum,float(fastq_filesize_sum)/1024/1024/1024)
-            userInput = raw_input('Sum of fastq files sizes are too big.  Is this ok? (y)es or (n)o ').lower()
+            userInput = input('Sum of fastq files sizes are too big.  Is this ok? (y)es or (n)o ').lower()
             if userInput == 'n':
                  raise Exception("Sum of fastq files sizes is too big for a {} sample!".format(sample_type))
         elif fastq_filesize_sum < 1073741824: # < 1GB
-            userInput = raw_input('Sum of fastq files sizes are too small.  Is this ok? (y)es or (n)o ').lower()
+            userInput = input('Sum of fastq files sizes are too small.  Is this ok? (y)es or (n)o ').lower()
             if userInput == 'n':
                  raise Exception("Sum of fastq files is too small for a {} sample!".format(sample_type))
     elif sample_type == 'custom_capture':
         if fastq_filesize_sum > 10737418240: # > 10GB
             print(fastq_filesize_sum,float(fastq_filesize_sum)/1024/1024/1024)
-            userInput = raw_input('Sum of fastq files sizes are too big.  Is this ok? (y)es or (n)o ').lower()
+            userInput = input('Sum of fastq files sizes are too big.  Is this ok? (y)es or (n)o ').lower()
             if userInput == 'n':
                  raise Exception("Sum of fastq files sizes is too big for a {} sample!".format(sample_type))
     else:
