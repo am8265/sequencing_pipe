22ad7ee52f5e395acffbf4422fabcd03c43e3d4a
joshbridgers <joshbridgers@gmail.com>
2018-01-30 17:24:24 -0500

    hotfix for prepT.priority addition

--- pipelineCron.py
+++ pipelineCron.py
@@ -1,3 +1,4 @@
+#!/nfs/goldstein/software/python3.6.1-x86_64_shared/bin/python
 import configparser
 import os
 from glob import glob
@@ -13,7 +14,7 @@ def main(config):
     query = """SELECT FCILLUMID
             FROM Flowcell f
             JOIN Lane l ON f.fcid=l.fcid
-            JOIN SampleT s ON s.dbid=l.dbid
+            JOIN prepT p ON p.dbid=l.dbid
             WHERE (PIPELINECOMPLETE = 0 AND COMPLETE = 1 AND FAIL != 1)
             GROUP BY l.FCID
             ORDER BY MIN(PRIORITY) ASC , FROM_UNIXTIME(SEQEND) ASC
@@ -39,15 +40,15 @@ def check_and_run_flowcell(fcillumid,config,database):
                                   run_info_dict['FCIllumID'])
 
     if glob(out_dir) == []:
-        cmd = ("{} {}/bcl2fastq_pipeline_wrapper.py --fcillumid {} -r"
-              ).format(config.get('programs','python36_program'),
-                       config.get('locs','scr_dir'),
-                       fcillumid)
+        src_dir=os.path.dirname(os.path.realpath(__file__))
+        print("we'll use '{}'".format(src_dir))
+        cmd = ("{}/bcl2fastq_pipeline_wrapper.py --fcillumid {} -r").format( src_dir, fcillumid)
         print(cmd)
         print()
         print('BCL2FASTQ STARTING')
         print()
-        os.system(cmd)
+        if os.system(cmd) != 0:
+            raise Exception("\n\ncouldn't run '{}'!".format(cmd))
     else:
         print('BCL DIR EXISTS!')
         print("***************************")
